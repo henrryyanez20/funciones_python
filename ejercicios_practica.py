061082416e4389bac0076d860d2d8647ef055314
Henrry <64995880+henrryyanez20@users.noreply.github.com>
2020-06-07 22:40:10 -0300

    Ej2 Listo trabajo modulos
    Ya funciona el llamado de modulos y el metodo random

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -5,7 +5,6 @@ Ejercicios de práctica
 ---------------------------
 Autor: Inove Coding School
 Version: 1.1
-
 Descripcion:
 Programa creado para que practiquen los conocimietos
 adquiridos durante la semana
@@ -15,6 +14,9 @@ __author__ = "Inove Coding School"
 __email__ = "alumnos@inove.com.ar"
 __version__ = "1.1"
 
+import random
+
+import mymodules
 
 def ej1():
     print('Comencemos a crear lo nuestro!')
@@ -29,33 +31,27 @@ def ej1():
     - ordenar
     - contar
     - buscar
-
     Importe el módulo a este programa/documento para su uso
     en el resto de los ejercicios
     '''
 
-
 def ej2():
     print("Jugando a los dados")
-
+    import mymodules
     '''
     Un dado común tiene 6 caras, 6 resultados posibles
     1 - 2 - 3 - 4 - 5 - 6
-
     Utilice la función "lista_aleatoria" para generar
     5 tiros de dados (una lista de 5 con resultados posibles
     de un dado)
-
     1)
     Utilice la función "ordenar" para ordenar la lista
     de números generados.
     Imprimir en pantalla la lista ordenada
-
     2)
     Importe el modulo "random" a este programa/documento
     Luego llame al método "shuffle" del módulo "random"
     para volver a mezclar la lista de tiros de dados.
-
     3)
     Utilice el método "sample" del módulo "random"
     para obtener al hazar 3 valores de la lista
@@ -63,6 +59,16 @@ def ej2():
     Imprima en pantalla dicha lista de 3 valores.
     '''
 
+    lista = mymodules.lista_aleatoria(1, 6, 5)
+    print('Lista aleatoria: ',lista)
+
+    mymodules.ordenar(lista)
+    print('Lista Ordenada : ',lista)
+
+    random.shuffle(lista)
+    print('Lista Mezclada : ', lista)
+
+    print('3 Valores Hazar: ',random.sample(lista,3)) 
 
 def ej3():
     print("Dominando la recursividad")
@@ -81,7 +87,6 @@ def ej3():
     Pueden ver más ejemplos y de que se trata el factorial en el
     siguiente link:
     https://www.disfrutalasmatematicas.com/numeros/factorial.html
-
     El objetivo es calcular el factorial utilizando recursividad,
     como pueden ver en el ejemplo anterior calcular el factorial de 5
     requiere además calcular el factorial de 4, y calcular el factorial de 4
@@ -90,7 +95,6 @@ def ej3():
     --> 4! = 4 * 3!
     --> 3! = 3 * 2 * 1
     --> 3! = 3 * 2!
-
     Les dejamos este ejercicio para que lo piensen, lo pueden dejar para el final
     Cualquier duda nos escriben en el Campus
     '''
@@ -104,20 +108,16 @@ def ej4():
     para generar una lista de 20 elementos, en un rango del 0 al 100 inclusive
     A continuación se solicitará que calcule el desvío estandar
     del la lista de números generaods.
-
     Para calcular el desvió estandar deberá aprovechar la función
     de "promedio", que en estadística al promedio se lo llama "media"
     Deberá calcular la sumatoria de la diferencia de todos los elementos
     de la lista respecto a la "media", dividirlo por la cantidad de elementos
     y calcular la raiz cuadrada.
-
     Les dejamos una página en donde explica el paso a paso del cálculo
     para que vayan viendo:
     https://es.khanacademy.org/math/probability/data-distributions-a1/summarizing-spread-distributions/a/calculating-standard-deviation-step-by-step
-
     El objetivo de este ejercicio es que practiquen la implementación
     matemática de algorítmos.
-
     Sugerencias:
     1 - Utilizar la función "promedio" para calcular la media
     2 - Para realizar la diferencia entre cada elemento de la lista
@@ -129,7 +129,6 @@ def ej4():
     3 - Utiliza el método "len" para obtener cuantos elementos
         hay en la lista "N".
     4 - Calcular la raiz cuadrada con el método de math correspondiente.
-
     '''
     # Mi implementación de desvió estandar a continuación:
 
@@ -148,17 +147,13 @@ def ej5():
     '''
     Este ejercicio representa ya un problema que forma parte de un juego
     Lo que se desea realizar es una parte del juego "la generala".
-
     Deberá realizar una lista para guardar 5 dados, guardar los números
     sacados en esa tirada de de dados (son 5 números del 1 al 6)
-
     1) El jugador tira la dados y sacar 5 números aleatorios, puede usar
     la función de "lista_aleatoria" para generar dichos números.
-
     2) Luego debe analizar los 5 números y ver cual es el número que
     más se repitio entre los 5 dados (debe usar la función max con
     la key de list.count)
-
     3) Una vez reconocido el número más repetido entre los 5, debe
     guardar en una lista aparte con esos números.
     Si por ejemplo salió 4-4-2-1-4, debe quedarse con esos tres "4"
@@ -169,7 +164,6 @@ def ej5():
     y el método "append" para agregar nuevos.
     Puede utilizar la función creada "buscar" para encontrar
     los índices del número que desea extraer.
-
     4) Debe volver a tira los dados, generar nuevos
     números aleatorios para aquellos dados reservados para
     tirar (dados_para_tirar). Es decir, que si tengo 2 elementos
@@ -177,7 +171,6 @@ def ej5():
     aleatorios nuevos.
     Otra opción es generar una "lista_generica" nueva y reemplazar
     los "dados_para_tirar".
-
     5) Luego de tirar nuevamente los datos, por ejemplo digamos
     que salieron los números: 4-1
     Debo volver a quedarme con el "4" ya que es el número que estoy
@@ -185,17 +178,15 @@ def ej5():
     Sino salió el "4" vuelvo a tirar todos los dados.
     Si salió un "4" me lo quedo y lo sacó de la lista y lo guardo
     en "dados_guardados".
-
     5) Debe repetir este proceso hasta que en su lista de "dados
     guardados" tenga "generala", es decir, 5 números iguales.
-
     '''
 
 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
     #ej1()
-    #ej2()
+    ej2()
     #ej3()
     #ej4()
     #ej5()
